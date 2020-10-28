c95f3a34b22a6ae60683b276b790180374bd565e
KiWi Developer <kiwifinance.bsc@gmail.com>
2020-10-29 05:35:08 +0600

    backto

---
+++ README.md
@@ -0,0 +1,15 @@
+# Pancake History
+
+Check it out live: [https://pancakeswap.info/](https://pancakeswap.info/).
+
+### To Start Development
+
+###### Installing dependencies
+```bash
+yarn
+```
+
+###### Running locally
+```bash
+yarn start
+```
---
+++ src/components/Attribution/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Attribution = () => (
+  <p className="attribution">
+    <a href="https://github.com/cheesemakerfarm/cheeseswap-info" rel="noopener noreferrer" target="_blank">
+      Github
+    </a>{' '}
+    |{' '}
+    <a href="https://cheeseswap.app" rel="noopener noreferrer" target="_blank">
+      CheeseSwap
+    </a>{' '}
+    |{' '}
+    <a href="https://gifer.com/en/1Atv" rel="noopener noreferrer" target="_blank">
+      GIF
+    </a>
+  </p>
+)
+
+export default Attribution

6083c6a7598f1291ea201aeb9ecd088dc8b600b1
SolProc <info@cheeseswap.app>
2021-01-08 16:53:20 +0600

    update

--- src/constants/index.js
+++ src/constants/index.js
@@ -21,7 +21,8 @@ export const OVERVIEW_TOKEN_BLACKLIST = [
     '0x2ad8184f181bc6e43cb838a42147987770c160be',
     '0x28e538167c34c15ac21395fb4d92c7c877d037c7',
     '0x236a130aebdf048da1976be6d17632cf27f229aa',
-    '0x7d6dafdebc81f691c83b1f9d91ea42a5754bdaf4'
+    '0x7d6dafdebc81f691c83b1f9d91ea42a5754bdaf4',
+    '0xcfd63197d764cd70d07bb607e6367ae0e869badd'
 ]
 
 // pair blacklist
@@ -29,7 +30,8 @@ export const PAIR_BLACKLIST = [
     '0xdb7cc7c7880ea6dbb1f42381a1cccf8f1b8171c1',
     '0x7cc41354109b648a15022eef6ac63191fa6f180e',
     '0x085493e164a5853ece035f2dbf3b5a66ebad427a',
-    '0xc8213bba1ad62283847f388d834b876994843b90'
+    '0xc8213bba1ad62283847f388d834b876994843b90',
+    '0xe6d6da24cade3c75f605e458abbf830698234d21'
 ]
 
 /**
